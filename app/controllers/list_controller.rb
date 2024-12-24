be0c950757676699b98fd10a4d511955cdc35ab3
tomo <kd1300547@st.kobedenshi.ac.jp>
2024-12-24 18:02:41 +0900

    [update]商品一覧画面でデータベースから商品すべてを取得して表示するように

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,6 +1,7 @@
 class ListController < ApplicationController
   before_action :require_login
     def index
+      @items = Item.all
       render("list/list")
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
 
   get "logout", to: "login#destroy", as: "logout"
 
+  resources :items, only: [:index]
 end
