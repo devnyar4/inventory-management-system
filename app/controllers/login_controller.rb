f13aa976e6ce95fe219b17c9d3264763b124a284
tomo <kd1300547@st.kobedenshi.ac.jp>
2024-12-24 14:56:26 +0900

    [feat]ログイン処理

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,6 +1,25 @@
 class LoginController < ApplicationController
-    def index
-      render("login/login")
+  def index
+    render("login/login")
+  end
+
+  def authentication
+    # login_id に基づいてユーザーを検索
+    user = User.find_by(login_id: params[:login_id])
+    
+    if user && user.password == params[:password]
+      # ログイン成功
+      session[:user_id] = user.id
+      redirect_to list_path, notice: 'ログインしました'
+    else
+      # ログイン失敗
+      flash.now[:alert] = 'ログインIDまたはパスワードが正しくありません'
+      render :login
     end
   end
-  
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path, notice: 'ログアウトしました'
+  end
+end
