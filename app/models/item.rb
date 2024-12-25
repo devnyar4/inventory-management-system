b394434d4aa441ceaaa22edb9f708edea451cf47
tomo <kd1300547@st.kobedenshi.ac.jp>
2024-12-25 09:28:00 +0900

    [update]バリデーションチェックのため、モデルにNot NULL, 加えて在庫は数値のみの負の数を受け付けない

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+    validates :item_name, presence: true
+    validates :inventory_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
