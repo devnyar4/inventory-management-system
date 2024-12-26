d07ca9c96cd17e6459d6b41f3eea05b8d0949df2
tomo <kd1300547@st.kobedenshi.ac.jp>
2024-12-26 23:26:46 +0900

    [ref]ディレクトリ構成変更のため削除

--- app/controllers/list_controller.rb
+++
@@ -1,8 +0,0 @@
-class ListController < ApplicationController
-  before_action :require_login
-    def index
-      @items = Item.all
-      render("list/list")
-    end
-  end
-  
--- app/controllers/update_controller.rb
+++
@@ -1,6 +0,0 @@
-class UpdateController < ApplicationController
-    before_action :require_login
-    def index
-      render("update/update")
-    end
-  end
